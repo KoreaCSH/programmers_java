dc70ff0e5bcfc501ea0e67e9521a0b26ff2df1e4
csh <tmdgjs7495@naver.com>
2023-08-28 15:03:34 +0900

    [Lv1] 가장 가까운 같은 글자

---
+++ Lv1/가장가까운같은글자.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class 가장가까운같은글자 {
+
+    public int[] solution(String s) {
+
+        int[] answer = new int[s.length()];
+
+        Map<Character, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < s.length(); i++) {
+            char tmp = s.charAt(i);
+            if(map.containsKey(tmp)) {
+                answer[i] = i - map.get(tmp);
+                map.put(tmp, i);
+            }
+            else {
+                map.put(tmp, i);
+                answer[i] = -1;
+            }
+        }
+
+        return answer;
+    }
+
+}
