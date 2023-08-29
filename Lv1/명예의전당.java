6324e425aab49ad588d0c530a01cfef926aa568a
csh <tmdgjs7495@naver.com>
2023-08-29 14:59:10 +0900

    [Lv1] 명예의 전당

---
+++ Lv1/명예의전당.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class 명예의전당 {
+
+    public int[] solution(int k, int[] score) {
+
+        int[] answer = new int[score.length];
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for(int i = 0; i < score.length; i++) {
+            pq.add(score[i]);
+
+            if(pq.size() > k) {
+                pq.poll();
+            }
+
+            answer[i] = pq.peek();
+        }
+
+        return answer;
+    }
+
+}
