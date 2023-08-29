0871abbafa6ecd8d0ede5923a2cb31dfe07a8de0
csh <tmdgjs7495@naver.com>
2023-08-29 22:54:19 +0900

    [refactor] 신고결과받기 - stream 활용

--- Lv1/신고결과받기.java
+++ Lv1/신고결과받기.java
@@ -4,12 +4,12 @@ public class 신고결과받기 {
 
     public int[] solution(String[] id_list, String[] report, int k) {
 
-        Map<String, Integer> reportCnt = new HashMap<>();
-        Map<String, List<String>> reportMap = new HashMap<>();
-        Map<String, Integer> mailCnt = new HashMap<>();
+        Map<String, Set<String>> reportMap = new HashMap<>();
+        Map<String, Integer> mailCnt = new LinkedHashMap<>();
 
         for(String s : id_list) {
             mailCnt.put(s, 0);
+            reportMap.put(s, new HashSet<String>());
         }
 
         for(int i = 0; i < report.length; i++) {
@@ -17,36 +17,18 @@ public class 신고결과받기 {
             String id = tmp[0];
             String reportedId = tmp[1];
 
-            if(!reportCnt.containsKey(reportedId)) {
-                reportCnt.put(reportedId, 1);
-                reportMap.put(reportedId, new ArrayList<String>());
-                reportMap.get(reportedId).add(id);
-            }
-            else {
-                if(reportMap.get(reportedId).contains(id)) {
-                    continue;
-                }
-                int cnt = reportCnt.get(reportedId);
-                reportCnt.put(reportedId, cnt+1);
-                reportMap.get(reportedId).add(id);
-            }
+            reportMap.get(reportedId).add(id);
         }
 
-        for(String s : reportCnt.keySet()) {
-            if(reportCnt.get(s) >= k) {
+        for(String s : reportMap.keySet()) {
+            if(reportMap.get(s).size() >= k) {
                 for(String id : reportMap.get(s)) {
                     mailCnt.put(id, mailCnt.get(id) + 1);
                 }
             }
         }
 
-        int[] answer = new int[id_list.length];
-
-        for(int i = 0; i < answer.length; i++) {
-            answer[i] = mailCnt.get(id_list[i]);
-        }
-
-        return answer;
+        return mailCnt.values().stream().mapToInt(i -> i.intValue()).toArray();
     }
 
 }
