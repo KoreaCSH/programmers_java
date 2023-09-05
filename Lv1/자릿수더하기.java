818e2635a6bdcd73adf8e8cdecd11b6cbb5fdd11
csh <tmdgjs7495@naver.com>
2023-09-05 23:19:46 +0900

    [Lv1] 자릿수 더하기

---
+++ Lv1/자릿수더하기.java
@@ -0,0 +1,15 @@
+public class 자릿수더하기 {
+
+    public int solution(int n) {
+
+        int ans = 0;
+        String tmp = String.valueOf(n);
+
+        for(char ch : tmp.toCharArray()) {
+            ans += ch -'0';
+        }
+
+        return ans;
+    }
+
+}
