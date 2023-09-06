4e57e47bd4cbba46d0415b5ea546b5fc4730f358
csh <tmdgjs7495@naver.com>
2023-09-06 15:14:33 +0900

    [Lv1] 없는 숫자 더하기

---
+++ Lv1/없는숫자더하기.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class 없는숫자더하기 {
+
+    public int solution(int[] numbers) {
+
+        Set<Integer> set = new HashSet<>();
+        int answer = 0;
+
+        for(int i = 0; i < numbers.length; i++) {
+            set.add(numbers[i]);
+        }
+
+        for(int i = 0; i < 10; i++) {
+            if(!set.contains(i)) {
+                answer += i;
+            }
+        }
+
+        return answer;
+    }
+
+}
