85bd5262318e1e20e44d234d528497ef2c0d1e3f
csh <tmdgjs7495@naver.com>
2023-09-13 10:09:39 +0900

    [Lv1] 부족한 금액 계산하기

---
+++ Lv1/부족한금액계산하기.java
@@ -0,0 +1,16 @@
+public class 부족한금액계산하기 {
+
+    public long solution(int price, int money, int count) {
+
+        long sum = 0;
+
+        for(int i = 0; i < count; i++) {
+            sum += (price * (i+1));
+        }
+
+        long answer = (sum - money > 0) ? sum - money : 0;
+
+        return answer;
+    }
+
+}
