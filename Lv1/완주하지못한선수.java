ff02cf36f3eebfcedcaa2dbd07e1d5a588fb45e0
csh <tmdgjs7495@naver.com>
2023-09-20 16:28:44 +0900

    [Lv1] 완주하지 못한 선수

---
+++ Lv1/완주하지못한선수.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class 완주하지못한선수 {
+
+    public String solution(String[] participant, String[] completion) {
+
+        String ans = "";
+        Map<String, Integer> map = new HashMap<>();
+
+        for(String s : participant) {
+            map.put(s, map.getOrDefault(s, 0) + 1);
+        }
+
+        for(String s : completion) {
+            map.put(s, map.get(s) - 1);
+        }
+
+        for(String s : participant) {
+            if(map.get(s) != 0) {
+                ans = s;
+                break;
+            }
+        }
+
+        return ans;
+    }
+
+}
