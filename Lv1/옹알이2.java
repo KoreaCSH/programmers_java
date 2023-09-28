b47117ae032e099ec59070b437815b7a888de003
csh <tmdgjs7495@naver.com>
2023-09-29 00:19:24 +0900

    [Lv1] : 옹알이2

---
+++ Lv1/옹알이2.java
@@ -0,0 +1,29 @@
+public class 옹알이2 {
+
+    public int solution(String[] babbling) {
+
+        int answer = 0;
+
+        for(String s : babbling) {
+            if(s.contains("ayaaya") ||
+                    s.contains("yeye") ||
+                    s.contains("woowoo") ||
+                    s.contains("mama")) {
+                continue;
+            }
+
+            s = s.replace("aya", " ");
+            s = s.replace("ye", " ");
+            s = s.replace("woo", " ");
+            s = s.replace("ma", " ");
+            s = s.replace(" ", "");
+
+            if(s.length() == 0) {
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+
+}
