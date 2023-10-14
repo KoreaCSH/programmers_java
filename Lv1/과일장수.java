74d9ff30e83061412f73fb6259e3f886f24cf8d9
csh <tmdgjs7495@naver.com>
2023-10-14 23:45:06 +0900

    [Lv1] 과일장수

---
+++ Lv1/과일장수.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class 과일장수 {
+
+    public int solution(int k, int m, int[] score) {
+
+        List<Integer> list = Arrays.stream(score)
+                .boxed()
+                .sorted(Collections.reverseOrder())
+                .collect(Collectors.toList());
+
+        int count = 0;
+        int answer = 0;
+
+        for(int i : list) {
+            count++;
+            if(count == m) {
+                answer += (i * m);
+                count = 0;
+            }
+        }
+
+        return answer;
+    }
+
+}
