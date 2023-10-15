e12dbf3b9742760990ca5a9c7d2543bb5ffa0827
csh <tmdgjs7495@naver.com>
2023-10-15 17:18:24 +0900

    [Lv1] 수박

---
+++ Lv1/수박.java
@@ -0,0 +1,28 @@
+public class 수박 {
+
+    public String solution(int n) {
+
+        StringBuilder sb = new StringBuilder();
+
+        if(n==1) {
+            return "수";
+        }
+        else if(n==2) {
+            return "수박";
+        }
+        else if(n % 2 == 1) {
+            sb.append("수");
+            for(int i = 0; i < n/2; i++) {
+                sb.append("박수");
+            }
+        }
+        else {
+            for(int i = 0; i < n/2; i++) {
+                sb.append("수박");
+            }
+        }
+
+        return sb.toString();
+    }
+
+}
