11122a33718948609420b58d7553134b3f8457d4
csh <tmdgjs7495@naver.com>
2023-10-20 19:44:31 +0900

    [Lv1] 푸드 파이터 대회

---
+++ Lv1/푸드파이터대회.java
@@ -0,0 +1,21 @@
+public class 푸드파이터대회 {
+
+    public String solution(int[] food) {
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 1; i < food.length; i++) {
+            if(food[i] < 2) {
+                continue;
+            }
+            for(int j = 0; j < food[i] / 2; j++) {
+                sb.append(i);
+            }
+        }
+
+        String ans = sb.toString() + "0" + sb.reverse().toString();
+
+        return ans;
+    }
+
+}
