1a95fec986735bf05027762f7e9672a9deec3fd6
csh <tmdgjs7495@naver.com>
2023-10-28 21:12:08 +0900

    [Lv1] K번째 수

---
+++ Lv1/K번째수.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class K번째수 {
+
+    public int[] solution(int[] array, int[][] commands) {
+
+        List<Integer> list;
+        int[] answer = new int[commands.length];
+
+        for(int i = 0; i < commands.length; i++) {
+            int begin = commands[i][0] - 1;
+            int end = commands[i][1] - 1;
+            int count = end - begin + 1;
+            int target = commands[i][2] - 1;
+
+            list = new ArrayList<>();
+            for(int j = 0; j < count; j++) {
+                list.add(array[begin++]);
+            }
+
+            Collections.sort(list);
+            answer[i] = list.get(target);
+        }
+
+        return answer;
+    }
+
+}
