86c560232b5f922374efe4c983047d2a2294d037
csh <tmdgjs7495@naver.com>
2023-10-31 21:43:37 +0900

    [Lv1] 최소직사각형

---
+++ Lv1/최소직사각형.java
@@ -0,0 +1,17 @@
+public class 최소직사각형 {
+
+    public int solution(int[][] sizes) {
+        int wMax = 0;
+        int hMax = 0;
+
+        for(int i = 0; i < sizes.length; i++) {
+            int w = Math.max(sizes[i][0], sizes[i][1]);
+            int h = Math.min(sizes[i][0], sizes[i][1]);
+            wMax = Math.max(wMax, w);
+            hMax = Math.max(hMax, h);
+        }
+
+        return wMax * hMax;
+    }
+
+}
