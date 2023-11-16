30589529d68fffa5a79c6cf61216387da4bef3e9
csh <tmdgjs7495@naver.com>
2023-11-16 14:39:32 +0900

    [Lv1] 3진법 뒤집기

---
+++ Lv1/삼진법뒤집기.java
@@ -0,0 +1,12 @@
+public class 삼진법뒤집기 {
+
+    public int solution(int n) {
+
+        String ternary = Integer.toString(n, 3);
+        StringBuilder ternaryReverse = new StringBuilder(ternary);
+
+        int answer = Integer.parseInt(ternaryReverse.reverse().toString(), 3);
+        return answer;
+    }
+
+}
