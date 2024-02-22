d5433946272fe0e5d405a95061198b7efc9bc8b5
csh <tmdgjs7495@naver.com>
2024-02-22 22:32:25 +0900

    [Lv1] 콜라츠추측

---
+++ Lv1/콜라츠추측.java
@@ -0,0 +1,28 @@
+package Lv1;
+
+public class 콜라츠추측 {
+
+    public int solution(int num) {
+
+        int count = 0;
+
+        while(num != 1) {
+
+            if(count >= 500) {
+                return -1;
+            }
+
+            if(num % 2 == 0) {
+                num /= 2;
+            }
+            else if (num % 2 == 1) {
+                num = num * 3 + 1;
+            }
+
+            count++;
+        }
+
+        return count;
+    }
+
+}
