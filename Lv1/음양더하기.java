b5594012a5d960f55c34329f1686306844c161e1
csh <tmdgjs7495@naver.com>
2024-04-18 23:31:00 +0900

    [Lv1] 음양 더하기

---
+++ Lv1/음양더하기.java
@@ -0,0 +1,20 @@
+package Lv1;
+
+public class 음양더하기 {
+
+    public int solution(int[] absolutes, boolean[] signs) {
+
+        int sum = 0;
+        for(int i = 0; i < absolutes.length; i++) {
+            if(signs[i]) {
+                sum += absolutes[i];
+            }
+            else {
+                sum -= absolutes[i];
+            }
+        }
+
+        return sum;
+    }
+
+}
